5a5fcfe95e4ebc1f7c7dd33c3f5849d0934fa48e
Mason Donahue <mdonahue@backstopsolutions.com>
2015-11-03 16:34:40 -0600

    Okay, use even-fuller 40-character key

--- manifests/repos.pp
+++ manifests/repos.pp
@@ -21,7 +21,7 @@ class dell::repos() inherits dell::params {
 
       # Dell APT Repos
       apt::key { 'dell-community':
-        key        => '1285491434D8786F',
+        key        => '42550ABD1E80D7C1BC0BAD851285491434D8786F',
         key_server => 'pool.sks-keyservers.net',
       }
 
